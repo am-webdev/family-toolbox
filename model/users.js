8db3f01f7ebeace08d66d8e32c77a99770a53832
Andreas Morgner <andreas.morgner@am-webdev.de>
2016-12-30 23:03:06 +0100

    adjusted user model

--- model/users.js
+++ model/users.js
@@ -4,8 +4,18 @@ var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
 	username: String,
-	email: String,
+	email: {
+		type: Date,
+		lowercase: true,
+		trim: true,
+		index: { unique: true }
+	},
 	password: String,
+	role: { 
+		type: String,
+		enum: ['Child', 'Parent']
+	},
+	admin: { type: Boolean, default: false },
 	created: { type: Date, default: Date.now },
 	updated: { type: Date, default: Date.now }
 });
