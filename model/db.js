c86af332e9a567d563146dad116c989e213773f3
Andreas Morgner <andreas.morgner@am-webdev.de>
2017-01-19 18:11:39 +0100

    check if env var exists

--- model/db.js
+++ model/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
-if (process.env.MONGOLAB_URI != '') {
-	mongoose.connect('mongodb://localhost/familytoolbox');
-} else {
+if (process.env.MONGOLAB_URI) {
 	mongoose.connect(process.env.MONGOLAB_URI);
+} else {
+	mongoose.connect('mongodb://localhost/familytoolbox');
 }
