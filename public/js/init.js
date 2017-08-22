3e073a1e908766e9f57cb5ce28e103c507f367e3
Andreas Morgner <andreas.morgner@am-webdev.de>
2017-08-22 22:40:23 +0200

    fixed datepicker for safari incl. MaterializeCSS #14

--- public/js/init.js
+++ public/js/init.js
@@ -2,4 +2,9 @@ $( document ).ready(function(){
    $(".button-collapse").sideNav();
    $(".dropdown-button").dropdown({ constrainWidth: false });
    $('.collapsible').collapsible();
+   $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: true, // Creates a dropdown of 15 years to control year
+    firstDay: 1
+  });
 })
